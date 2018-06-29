d42c366d0941a53b475411a710b39369e3e9643f
Daniel Son <sondaniel96@gmail.com>
2018-06-29 00:06:14 -0700

    added angular material created side-navbar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SideNavbarComponent } from './side-navbar/side-navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SideNavbarComponent
   ],
   imports: [
     BrowserModule
